e9ee3374bb74d03a0ed2a65bd7501542d982b51e
carolinasolana <carolina.solana@icloud.com>
2023-11-11 19:17:32 -0500

    TEHE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TESTYZESTY.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TESTYZESTY.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.tuning;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous
 public class TESTYZESTY extends LinearOpMode {
