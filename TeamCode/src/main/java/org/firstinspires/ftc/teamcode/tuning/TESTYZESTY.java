ba4ce22437aeb0754c02706a6cec979d5d6989c6
Grace <dug29contacts@gmail.com>
2023-11-11 21:30:59 -0500

    ehgfhbfk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TESTYZESTY.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TESTYZESTY.java
@@ -30,12 +30,7 @@ public class TESTYZESTY extends LinearOpMode {
 
 
         waitForStart();
-
-
-
-        //test function]
-        public void moveForward(double distance, int power) {
-            RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -56,6 +51,31 @@ public class TESTYZESTY extends LinearOpMode {
             LFMotor.setPower(0);
             RBMotor.setPower(0);
             LBMotor.setPower(0);
+
+
+        //test function]
+//        public void moveForward(double distance, int power) {
+//            RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            RFMotor.setTargetPosition((int) distance);
+//            RBMotor.setTargetPosition((int) distance);
+//            LFMotor.setTargetPosition((int) distance);
+//            LBMotor.setTargetPosition((int) distance);
+//            RFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            LFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            RBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            LBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            RFMotor.setPower(power);
+//            RBMotor.setPower(power);
+//            LFMotor.setPower(power);
+//            LBMotor.setPower(power);
+//            while (RFMotor.isBusy() || RBMotor.isBusy() || LFMotor.isBusy() || LBMotor.isBusy() ||false) {}
+//            RFMotor.setPower(0);
+//            LFMotor.setPower(0);
+//            RBMotor.setPower(0);
+//            LBMotor.setPower(0);
 
         }
 
