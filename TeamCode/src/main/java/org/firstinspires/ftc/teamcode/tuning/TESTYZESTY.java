556e65d361127ff44e43f88f918a8f74ee4f8c8e
Grace <dug29contacts@gmail.com>
2023-11-11 19:11:50 -0500

    Added imports from last repository

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TESTYZESTY.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TESTYZESTY.java
@@ -1,5 +1,31 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
-public class TESTYZESTY {
-    //HI GRACIEPIE
-}
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class TESTYZESTY extends LinearOpMode {
+
+    DcMotor RFMotor;
+    DcMotor LFMotor;
+    DcMotor RBMotor;
+    DcMotor LBMotor;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+
+        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // ticks per revolution
+        RFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+    }
