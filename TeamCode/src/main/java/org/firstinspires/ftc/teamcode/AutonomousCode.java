3f400727c09e24d88d3ec4a073b95da942657ba9
Grace <dug29contacts@gmail.com>
2023-11-16 19:13:09 -0500

    ehgfhbfk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCode.java
@@ -0,0 +1,112 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous
+public class AutonomousCode extends LinearOpMode {
+
+    DcMotor RFMotor;
+    DcMotor LFMotor;
+    DcMotor RBMotor;
+    DcMotor LBMotor;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+
+        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // ticks per revolution
+        RFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        waitForStart();
+
+
+        moveForward(0.5, 1000);
+        RightTurn(0.5, 500);
+        LeftTurn(0.5, 500);
+    }
+        //test function]
+        public void moveForward(double power, double distance) {
+            RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            RFMotor.setTargetPosition((int) distance);
+            RBMotor.setTargetPosition((int) distance);
+            LFMotor.setTargetPosition((int) distance);
+            LBMotor.setTargetPosition((int) distance);
+            RFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            LFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            LBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RFMotor.setPower(power);
+            RBMotor.setPower(power);
+            LFMotor.setPower(power);
+            LBMotor.setPower(power);
+            while (RFMotor.isBusy() || RBMotor.isBusy() || LFMotor.isBusy() || LBMotor.isBusy() ||false) {}
+            RFMotor.setPower(0);
+            LFMotor.setPower(0);
+            RBMotor.setPower(0);
+            LBMotor.setPower(0);
+        }
+    public void RightTurn(double power, double distance) {
+        RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RFMotor.setTargetPosition((int) distance);
+        RBMotor.setTargetPosition((int) distance);
+        LFMotor.setTargetPosition((int) distance);
+        LBMotor.setTargetPosition((int) distance);
+        RFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RFMotor.setPower(-power);
+        RBMotor.setPower(-power);
+        LFMotor.setPower(power);
+        LBMotor.setPower(power);
+        while (RFMotor.isBusy() || RBMotor.isBusy() || LFMotor.isBusy() || LBMotor.isBusy() ||false) {}
+        RFMotor.setPower(0);
+        LFMotor.setPower(0);
+        RBMotor.setPower(0);
+        LBMotor.setPower(0);
+    }
+    public void LeftTurn(double power, double distance) {
+        RFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RFMotor.setTargetPosition((int) distance);
+        RBMotor.setTargetPosition((int) distance);
+        LFMotor.setTargetPosition((int) distance);
+        LBMotor.setTargetPosition((int) distance);
+        RFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LFMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LBMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RFMotor.setPower(power);
+        RBMotor.setPower(power);
+        LFMotor.setPower(-power);
+        LBMotor.setPower(-power);
+        while (RFMotor.isBusy() || RBMotor.isBusy() || LFMotor.isBusy() || LBMotor.isBusy() ||false) {}
+        RFMotor.setPower(0);
+        LFMotor.setPower(0);
+        RBMotor.setPower(0);
+        LBMotor.setPower(0);
+    }
+
+    }
+
+
