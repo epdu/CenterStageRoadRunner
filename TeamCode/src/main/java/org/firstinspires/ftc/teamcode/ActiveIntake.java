52aedf7aba16df244c300d08edb791ab32b78388
Grace <dug29contacts@gmail.com>
2023-11-17 21:23:26 -0500

    ehgfhbfk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActiveIntake.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class ActiveIntake extends OpMode {
+    DcMotor Intake;
+    boolean move = false;
+
+
+    @Override
+    public void init (){
+        Intake = hardwareMap.get(DcMotor.class, "Intake");
+    }
+
+    @Override
+    public void loop(){
+        if (gamepad1.y && !move) {
+
+        }
+    }
+}
