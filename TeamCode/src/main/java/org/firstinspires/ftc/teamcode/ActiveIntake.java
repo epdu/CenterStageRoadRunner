760ab4186db0bd9faa9ac19451f53cc07cbf0f10
Grace <dug29contacts@gmail.com>
2023-11-17 23:29:50 -0500

    ehgfhbfk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActiveIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActiveIntake.java
@@ -3,30 +3,44 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class ActiveIntake extends OpMode {
     DcMotor Intake;
+    public Servo IntakeServo;
     boolean move = false;
     public float speedMultiplier = 1;
     public float speedMultiplier1 = 0.2f;
 
 
     @Override
-    public void init (){
+    public void init () {
         Intake = hardwareMap.get(DcMotor.class, "Intake");
+        IntakeServo = hardwareMap.get(Servo.class, "IntakeServo");
     }
+
 
     @Override
     public void loop(){
-        if (gamepad1.y && !move) {
         moveIntake();
+        if (gamepad2.b && !move) {
+            IntakeServo.setPosition(0.5);
+        }else if (gamepad2.x && !move){
+            IntakeServo.setPosition(0);
+        }else {
+            ejectPixel();
         }
     }
 
     public void moveIntake(){
-        double intake = gamepad1.right_trigger;
+        double intake = gamepad2.right_trigger;
         Intake.setPower(intake*speedMultiplier);
+    }
+
+    public void ejectPixel(){
+        double intake = gamepad2.left_trigger;
+        Intake.setPower(intake*speedMultiplier1);
     }
 
 }
