f3d28f6fe536bbdd61e7979cee27dc8a04d02fe5
david <epdu@temple.edu>
2023-11-21 01:07:26 -0500

    centerfield

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
@@ -68,10 +68,10 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY - rotX - rx) / denominator;
-            double backLeftPower = (rotY + rotX - rx) / denominator;
-            double frontRightPower = (rotY + rotX + rx) / denominator;
-            double backRightPower = (rotY - rotX + rx) / denominator;
+            double frontLeftPower = (rotY + rotX - rx) / denominator;
+            double backLeftPower = (rotY - rotX - rx) / denominator;
+            double frontRightPower = (rotY - rotX + rx) / denominator;
+            double backRightPower = (rotY + rotX + rx) / denominator;
 
             LFMotor.setPower(frontLeftPower);
             LBMotor.setPower(backLeftPower);
