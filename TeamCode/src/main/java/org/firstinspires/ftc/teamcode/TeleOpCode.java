3cddb8645ee60b3ca0535057cad45067a45ca46e
david <epdu@temple.edu>
2023-11-23 16:40:31 -0500

    centerfield

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCode.java
@@ -6,6 +6,13 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
 public class TeleOpCode extends OpMode {
@@ -30,8 +37,8 @@ public class TeleOpCode extends OpMode {
     private DcMotorEx motor;
     DcMotor Intake;
     public Servo IntakeServo;
-    public float speedMultiplier2 = 1;
-    public float speedMultiplier1 = 0.8f;
+    public float speedMultiplier2 = 0.8f;
+    public float speedMultiplier1 = 1;
     @Override
     public void init() {
         RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
@@ -41,6 +48,9 @@ public class TeleOpCode extends OpMode {
 
         RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Retrieve the IMU from the hardware map
+
 
 //        liftMotorL = hardwareMap.get(DcMotor.class, "liftMotorL");
 //
@@ -63,7 +73,8 @@ public class TeleOpCode extends OpMode {
     }
     @Override
     public void loop(){
-        moveDriveTrain();
+//        moveDriveTrain();
+        FieldCentricDriveTrain();
 //        if (gamepad2.y && !move) {
 //            moveSlideToPosition(POSITION_Y);
 //        }  else if (gamepad2.a && !move) {
@@ -123,6 +134,49 @@ public class TeleOpCode extends OpMode {
 
 
     }
+
+    public void FieldCentricDriveTrain() {
+        double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = - gamepad1.left_stick_x;
+        double rx = - gamepad1.right_stick_x;
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+        // This button choice was made so that it is hard to hit on accident,
+        // it can be freely changed based on preference.
+        // The equivalent button is start on Xbox-style controllers.
+        if (gamepad1.options) {
+            imu.resetYaw();
+        }
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        // Rotate the movement direction counter to the bot's rotation
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        LFMotor.setPower(frontLeftPower);
+        LBMotor.setPower(backLeftPower);
+        RFMotor.setPower(frontRightPower);
+        RBMotor.setPower(backRightPower);
+
+
+    }
 //    private void moveSlideToPosition(int targetPosition) {
 //        liftMotorL.setTargetPosition(targetPosition);
 //        liftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -160,12 +214,12 @@ public class TeleOpCode extends OpMode {
 
     public void moveIntake(boolean dpad_up, boolean dpad_down){
         double intake = gamepad2.right_trigger;
-        Intake.setPower(-intake*speedMultiplier);
+        Intake.setPower(-intake*speedMultiplier1);
     }
 
     public void ejectPixel(){
         double intake = gamepad2.left_trigger;
-        Intake.setPower(intake*speedMultiplier1);
+        Intake.setPower(intake*speedMultiplier2);
     }
 
 }
