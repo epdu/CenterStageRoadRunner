77259d052842690585e62006a853daea80613b03
david <epdu@temple.edu>
2023-12-09 02:18:52 -0500

    epdu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraFusedPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraFusedPID.java
@@ -1,4 +1,5 @@
-package org.firstinspires.ftc.teamcode;
+
+/*package org.firstinspires.ftc.teamcode;
 //package org.firstinspires.ftc.teamcode.OpModes.Angle_PID_Tutorial;
 
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -43,6 +44,7 @@ public class CameraFusedPID extends LinearOpMode {
 
     private OpenCvCamera controlHubCam;  // Use OpenCvCamera class from FTC SDK
     /** MAKE SURE TO CHANGE THE FOV AND THE RESOLUTIONS ACCORDINGLY **/
+/*
     private static final int CAMERA_WIDTH = 640; // width  of wanted camera resolution
     private static final int CAMERA_HEIGHT = 360; // height of wanted camera resolution
     private static final double FOV = 40;
@@ -210,3 +212,4 @@ public class CameraFusedPID extends LinearOpMode {
 
 
 }
+*/
