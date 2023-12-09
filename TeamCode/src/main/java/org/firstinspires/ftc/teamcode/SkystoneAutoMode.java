48b7c9b8c5b161383c25ced8dfd5e6b664af2d21
epdu <epdu@temple.edu>
2023-12-09 01:24:37 -0500

    epdu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneAutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneAutoMode.java
@@ -1,12 +1,26 @@
 //package org.wolfcorp.cv.tutorial;
 package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name="Skystone Detecotor", group="Auto")
 public class SkystoneAutoMode extends LinearOpMode {
@@ -16,11 +30,32 @@ public class SkystoneAutoMode extends LinearOpMode {
         int cameraMonitorViewId = hardwareMap.appContext
                 .getResources().getIdentifier("cameraMonitorViewId",
                         "id", hardwareMap.appContext.getPackageName());
-        phoneCam = OpenCvCameraFactory.getInstance()
-                .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
         SkystoneDetector detector = new SkystoneDetector(telemetry);
         phoneCam.setPipeline(detector);
-        phoneCam.openCameraDeviceAsync(() -> phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT));
+
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                phoneCam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });        
+ /*              () -> {
+                   return phoneCam.startStreaming(320, 240, org.openftc.easyopencv.OpenCvCameraRotation.UPRIGHT);
+               }
+ */
+
+
+
 
         waitForStart();
         switch (detector.getLocation()) {
