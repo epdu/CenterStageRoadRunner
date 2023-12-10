9308b85be433ce925965fcc3b083dbaebba20354
epdu <epdu@temple.edu>
2023-12-09 23:09:45 -0500

    epdu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetector.java
@@ -37,19 +37,40 @@ public class SkystoneDetector extends OpenCvPipeline {
     @Override
     public Mat processFrame(Mat input) {
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+
+
+        Scalar lowHSV = new Scalar(123, 25, 31); // lower bound HSV for blue tested by cone 223 25 31
+        Scalar highHSV =  new Scalar(143, 255, 255); // higher bound HSV for blue
+
 /*
-        Scalar lowHSV = new Scalar(23, 50, 70); // lower bound HSV for yellow
-        Scalar highHSV = new Scalar(32, 255, 255);  // higher bound HSV for yellow
+        Scalar lowHSV = new Scalar(1, 98, 34); // lower bound HSV for red tested by cone 10, 98, 34
+        Scalar highHSV =  new Scalar(20, 255, 255); // higher bound HSV for red
+
+        Scalar lowHSV = new Scalar(200, 32, 49); // lower bound HSV for green tested by pixel 111, 32, 49
+        Scalar highHSV =  new Scalar(121, 255, 255); // higher bound HSV for green
+
+        Scalar lowHSV = new Scalar(x, 19, 76); // lower bound HSV for purple pixel 284, 13, 55
+        Scalar highHSV = new Scalar(x, 255, 255);  // higher bound HSV for
+
+        Scalar lowHSV = new Scalar(0, 0, 100); // lower bound HSV for white pixel 0, 0, 100
+        Scalar highHSV = new Scalar(10, 255, 255);  // higher bound HSV for
+                Scalar lowHSV = new Scalar(33, 99, 65); // lower bound HSV for yellow pixel 43, 99, 65
+        Scalar highHSV = new Scalar(53, 255, 255);  // higher bound HSV for
+
+        Now you take [H-10, 100,100] and [H+10, 255, 255] as the lower bound and upper bound respectively.
+        Apart from this method, you can use any image editing tools like GIMP or any online converters to find these values,
+        but don't forget to adjust the HSV ranges.
+
 */
 
 /*
         Scalar lowHSV = new Scalar(100, 100, 100);  // lower bound HSV for red
         Scalar highHSV =  new Scalar(180, 255, 255);  // higher bound HSV for red
 */
-        Scalar lowHSV = new Scalar(95, 110, 50); // lower bound HSV for blue
+
+/*        Scalar lowHSV = new Scalar(95, 110, 50); // lower bound HSV for blue
         Scalar highHSV =  new Scalar(150, 245, 255); // higher bound HSV for blue
-
-
+ */
 /*
         Scalar lowHSV = new Scalar(89, 67, 61); // lower bound HSV for green
         Scalar highHSV = new Scalar(83, 43, 83); // higher bound HSV for green
