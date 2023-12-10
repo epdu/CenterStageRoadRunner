702122324a78a3c5427afb3141dc746d7d3a01e4
epdu <epdu@temple.edu>
2023-12-10 17:22:29 -0500

    epdu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomousv1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomousv1.java
@@ -200,8 +200,6 @@ public class Autonomousv1 extends LinearOpMode {
                 RevHubOrientationOnRobot.UsbFacingDirection.UP));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
 
-
-
         if (gamepad1.options) {
             imu.resetYaw();
         }
@@ -218,13 +216,13 @@ public class Autonomousv1 extends LinearOpMode {
         }
 */
 
-// set the distanct from frot of robot to the block of game element
-/*  Using the specs from the motor, you would need to find the encoder counts per revolution (of the output shaft).
+/*
+set the distanct from frot of robot to the block of game element
+Using the specs from the motor, you would need to find the encoder counts per revolution (of the output shaft).
      Then, you know that corresponds to 360 degrees of wheel rotation, which means the distance travelled is the circumference
       of the wheel (2 * pi * r_wheel). To figure out how many encoder ticks correspond to the distance you wanna go,
       just multiply the distance by the counts / distance you calculated above. Hope that helps!
-// 11.87374348
-//537 per revolution 11.87374348 inch
+11.87374348 537 per revolution 11.87374348 inch
 */
         distanceInInch=24;//number in unit of inch
         distanceInInchDouble=(double)(distanceInInch*537/(Math.PI * wheelDiameterInInches));
@@ -240,10 +238,9 @@ public class Autonomousv1 extends LinearOpMode {
         if (gamepad1.options) {
             imu.resetYaw();
         }
-
         botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
-        /*
-// run until the end of the match (driver presses STOP)
+/*
+        run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             double leftReading = LeftSensor.getDistance(DistanceUnit.INCH);
             double rightReading = RightSensor.getDistance(DistanceUnit.INCH);
@@ -254,23 +251,17 @@ public class Autonomousv1 extends LinearOpMode {
 */
 }
     private void initOpenCV() {
-
         // Create an instance of the camera
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-
         // Use OpenCvCameraFactory class from FTC SDK to create camera instance
         controlHubCam = OpenCvCameraFactory.getInstance().createWebcam(
                 hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
-
         controlHubCam.setPipeline(new opencv.YellowBlobDetectionPipeline());
-
         controlHubCam.openCameraDevice();
         controlHubCam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
     }
-
 public void  findteamPropLocationsbyDistanceSensors(){
-
         double leftReading = LeftSensor.getDistance(DistanceUnit.INCH);
         double rightReading = RightSensor.getDistance(DistanceUnit.INCH);
         telemetry.addData("Left", leftReading);
@@ -295,84 +286,196 @@ public void  findteamPropLocationsbyDistanceSensors(){
             telemetry.addData("Right", rightReading);
             telemetry.addData("teamPropLocations", teamPropLocations);
             telemetry.update();
+        }
+}
+public void  findteamPropLocationsopencv(){
+//////////////////
+        double leftReading = LeftSensor.getDistance(DistanceUnit.INCH);
+        double rightReading = RightSensor.getDistance(DistanceUnit.INCH);
+        telemetry.addData("Left", leftReading);
+        telemetry.addData("Right", rightReading);
+        telemetry.update();
+// L=24.68+- 2, Center=30.56+-2
+        if(leftReading > 23 && leftReading < 28 && rightReading > 40){
+            teamPropLocations="Left";
+            telemetry.addData("Left", leftReading);
+            telemetry.addData("Right", rightReading);
+            telemetry.addData("teamPropLocations", teamPropLocations);
+            telemetry.update();
+        } else if ( leftReading > 28 && leftReading < 40 && rightReading > 28 && rightReading < 40) {
+            teamPropLocations = "Center";
+            telemetry.addData("Left", leftReading);
+            telemetry.addData("Right", rightReading);
+            telemetry.addData("teamPropLocations", teamPropLocations);
+            telemetry.update();
+        } else if( leftReading > 40 && rightReading > 40) {
+            teamPropLocations = "Right";
+            telemetry.addData("Left", leftReading);
+            telemetry.addData("Right", rightReading);
+            telemetry.addData("teamPropLocations", teamPropLocations);
+            telemetry.update();
+        }
+
+    }
+/////////////////////////////
+class BlueTeamPropDetectionPipeline extends OpenCvPipeline {
+        @Override
+        public Mat processFrame(Mat input) {
+            // Preprocess the frame to detect yellow regions
+            Mat yellowMask = preprocessFrame(input);
+            // Find contours of the detected yellow regions
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(yellowMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            // Find the largest yellow contour (blob)
+            MatOfPoint largestContour = findLargestContour(contours);
+
+            if (largestContour != null) {
+                // Draw a red outline around the largest detected object
+                Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(255, 0, 0), 2);
+                // Calculate the width of the bounding box
+                width = calculateWidth(largestContour);
+
+                // Display the width next to the label
+                String widthLabel = "Width: " + (int) width + " pixels";
+                Imgproc.putText(input, widthLabel, new Point(cX + 10, cY + 20), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+                //Display the Distance
+                String distanceLabel = "Distance: " + String.format("%.2f", getDistance(width)) + " inches";
+                Imgproc.putText(input, distanceLabel, new Point(cX + 10, cY + 60), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+                // Calculate the centroid of the largest contour
+                Moments moments = Imgproc.moments(largestContour);
+                cX = moments.get_m10() / moments.get_m00();
+                cY = moments.get_m01() / moments.get_m00();
+                // Draw a dot at the centroid
+                String label = "(" + (int) cX + ", " + (int) cY + ")";
+                Imgproc.putText(input, label, new Point(cX + 10, cY), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+                Imgproc.circle(input, new Point(cX, cY), 5, new Scalar(0, 255, 0), -1);
+
+                if(cX > 0 && cX < 365 ){
+                    teamPropLocations="Left";
+                    telemetry.addData("Left", cX);
+                    telemetry.addData("teamPropLocations", teamPropLocations);
+                    telemetry.update();
+                } else if ( cX > 460 && cX < 820) {
+                    teamPropLocations = "Center";
+                    telemetry.addData("Center", cX);
+                    telemetry.addData("teamPropLocations", teamPropLocations);
+                    telemetry.update();
+                } else if( cX > 915 && cX < 1280) {
+                    teamPropLocations = "Right";
+                    telemetry.addData("Right",cX);
+                    telemetry.addData("teamPropLocations", teamPropLocations);
+                    telemetry.update();
+                }
+            }
+            return input;
+        }
+        private Mat preprocessFrame(Mat frame) {
+            Mat hsvFrame = new Mat();
+            Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);
+            Scalar lowerYellow = new Scalar(1, 98, 34); // lower bound HSV for blue tested by blue cone 223 25 31
+            Scalar upperYellow = new Scalar(30, 255, 255);
+  /*
+            Scalar lowHSV = new Scalar(123, 25, 31); // lower bound HSV for blue tested by cone 223 25 31
+            Scalar highHSV =  new Scalar(143, 255, 255); // higher bound HSV for blue  214, 34, 28       100-140
+
+            Scalar lowHSV = new Scalar(1, 98, 34); // lower bound HSV for red tested by cone 10, 98, 34
+            Scalar highHSV =  new Scalar(20, 255, 255); // higher bound HSV for red
+
+            Scalar lowHSV = new Scalar(200, 32, 49); // lower bound HSV for green tested by pixel 111, 32, 49
+            Scalar highHSV =  new Scalar(121, 255, 255); // higher bound HSV for green
+
+            Scalar lowHSV = new Scalar(x, 19, 76); // lower bound HSV for purple pixel 284, 13, 55
+            Scalar highHSV = new Scalar(x, 255, 255);  // higher bound HSV for
+
+            Scalar lowHSV = new Scalar(0, 0, 100); // lower bound HSV for white pixel 0, 0, 100
+            Scalar highHSV = new Scalar(10, 255, 255);  // higher bound HSV for
+            Scalar lowHSV = new Scalar(33, 99, 65); // lower bound HSV for yellow pixel 43, 99, 65
+            Scalar highHSV = new Scalar(53, 255, 255);  // higher bound HSV for
+
+            Now you take [H-10, 100,100] and [H+10, 255, 255] as the lower bound and upper bound respectively.
+            Apart from this method, you can use any image editing tools like GIMP or any online converters to find these values,
+            but don't forget to adjust the HSV ranges.
+
+            Scalar lowHSV = new Scalar(100, 100, 100);  // lower bound HSV for red
+            Scalar highHSV =  new Scalar(180, 255, 255);  // higher bound HSV for red
+            Scalar lowHSV = new Scalar(95, 110, 50); // lower bound HSV for blue
+            Scalar highHSV =  new Scalar(150, 245, 255); // higher bound HSV for blue
+
+            Scalar lowHSV = new Scalar(89, 67, 61); // lower bound HSV for green
+            Scalar highHSV = new Scalar(83, 43, 83); // higher bound HSV for green
+
+            Scalar lowHSV = new Scalar(23, 50, 70); // lower bound HSV for yellow
+            Scalar highHSV = new Scalar(32, 255, 255);  // higher bound HSV for yellow
+
+            Scalar lowHSV = new Scalar(100, 100, 100);  // lower bound HSV for red
+            Scalar highHSV =  new Scalar(180, 255, 255);  // higher bound HSV for red
+
+            Scalar lowHSV = new Scalar(95, 110, 50); // lower bound HSV for blue
+            Scalar highHSV =  new Scalar(150, 245, 255); // higher bound HSV for blue
+
+            Scalar lowHSV = new Scalar(89, 67, 61); // lower bound HSV for green
+            Scalar highHSV = new Scalar(83, 43, 83); // higher bound HSV for green
+*/
+
+            Mat yellowMask = new Mat();
+            Core.inRange(hsvFrame, lowerYellow, upperYellow, yellowMask);
+
+            Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+            Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_OPEN, kernel);
+            Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_CLOSE, kernel);
+
+            return yellowMask;
+        }
+
+        private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
+            double maxArea = 0;
+            MatOfPoint largestContour = null;
+
+            for (MatOfPoint contour : contours) {
+                double area = Imgproc.contourArea(contour);
+                if (area > maxArea) {
+                    maxArea = area;
+                    largestContour = contour;
+                }
+            }
+
+            return largestContour;
+        }
+        private double calculateWidth(MatOfPoint contour) {
+            Rect boundingRect = Imgproc.boundingRect(contour);
+            return boundingRect.width;
         }
 
 }
-
-    public void  findteamPropLocationsopencv(){
-
-        double leftReading = LeftSensor.getDistance(DistanceUnit.INCH);
-        double rightReading = RightSensor.getDistance(DistanceUnit.INCH);
-        telemetry.addData("Left", leftReading);
-        telemetry.addData("Right", rightReading);
-        telemetry.update();
-// L=24.68+- 2, Center=30.56+-2
-        if(leftReading > 23 && leftReading < 28 && rightReading > 40){
-            teamPropLocations="Left";
-            telemetry.addData("Left", leftReading);
-            telemetry.addData("Right", rightReading);
-            telemetry.addData("teamPropLocations", teamPropLocations);
-            telemetry.update();
-        } else if ( leftReading > 28 && leftReading < 40 && rightReading > 28 && rightReading < 40) {
-            teamPropLocations = "Center";
-            telemetry.addData("Left", leftReading);
-            telemetry.addData("Right", rightReading);
-            telemetry.addData("teamPropLocations", teamPropLocations);
-            telemetry.update();
-        } else if( leftReading > 40 && rightReading > 40) {
-            teamPropLocations = "Right";
-            telemetry.addData("Left", leftReading);
-            telemetry.addData("Right", rightReading);
-            telemetry.addData("teamPropLocations", teamPropLocations);
-            telemetry.update();
-        }
-
-    }
-    public void  dropPurplrPixel(){
+private static double getDistance(double width){
+        double distance = (objectWidthInRealWorldUnits * focalLength) / width;
+        return distance;
+}
+public void  dropPurplrPixel(){
 
         if(teamPropLocations == "Left"){
-            moveForward(0.2, 16);
-            StrafingLeft(0.2, 12);
-
-
+            moveBackward(0.2, 40);
+//or select turn to right , gyroTurn
+//            StrafingRight(0.2, 12);
+//            RightTurn(0.2, 12);
+            gyroTurn(0.2, - 90);
         } else if ( teamPropLocations == "Right") {
-            moveForward(0.2, 16);
-            StrafingRight(0.2, 12);
-
-        } else {
-            teamPropLocations = "Right";
-            moveForward(0.2, 30);
+            moveBackward(0.2, 30);
+//or select turn to left
+//            StrafingLeft(0.2, 12);
+//            LeftTurn(0.2, 12);
+            gyroTurn(0.2,  90);
+        } else if ( teamPropLocations == "Center") {
+            moveBackward(0.2, 46);
         }
-
 //        checkTeamPropColors();
 //        lineUPteamProp();
-    }
+}
     public void  checkTeamPropColors(){ }
-    ;
     public void  lineUPteamProp(){ }
     //////////////////
 
-
-    /*
-            moveForward(0.2, distanceInInchDouble);
-            sleep(1000);
-            moveBackward(0.2, distanceInInchDouble);
-            sleep(1000);
-
-     */
-      /*  StrafingRight(0.2, 1000);
-        sleep(1000);//strafing left
-        moveForward(0.5, 1000);
-        sleep(2000);
-        sleep(2000);
-
-        RightTurn(0.2, 500);
-        sleep(1000);
-        LeftTurn(0.2, 500);
-        sleep(1000);
-        StrafingLeft(0.2, 1000);
-        sleep(1000);//strafing left
-*/
     public void gyroTurn(double power, double degrees){ //right is negative
         if(opModeIsActive()){
             double gyroinitial = newGetHeading();
@@ -382,7 +485,6 @@ public void  findteamPropLocationsbyDistanceSensors(){
                     LBMotor.setPower(-power);
                     LFMotor.setPower(-power);
                     RBMotor.setPower(power);
-
 
                     updates = Double.toString(newGetHeading());
                     telemetry.addData("Heading", newGetHeading());
