c48f00ebc73944773a19cdf585f62bdb8340c115
epdu <epdu@temple.edu>
2023-12-19 15:35:03 -0500

    epdu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -3,12 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-
 
 @Autonomous(name="Kickoff Presentation: Gyro", group="Pushbot")
 public class Gyro extends LinearOpMode{
