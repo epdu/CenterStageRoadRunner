c03e25951cc4edcf11c45f187a0aa8fdfa73c5ef
epdu <epdu@temple.edu>
2024-02-06 20:10:48 -0500

    servotesty

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servogobrrrr.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+@TeleOp
+public class servogobrrrr extends OpMode {
+    Servo ArmR;
+    Servo ArmL;
+    boolean move = false;
+
+    @Override
+    public void init() {
+        ArmL = hardwareMap.get(Servo.class, "ArmL");
+        ArmR = hardwareMap.get(Servo.class, "ArmR");
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad2.dpad_up && !move) { //up
+            ArmR.setPosition(0);
+            ArmL.setPosition(0);
+        }
+        if (gamepad2.dpad_down && !move) { //down
+            ArmL.setPosition(0.8);
+            ArmR.setPosition(0.8);
+        }
+    }
+}
