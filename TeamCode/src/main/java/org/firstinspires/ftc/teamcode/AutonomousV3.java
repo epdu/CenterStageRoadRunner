6bd354ae650f9abfc6358db153d007a8747e5ee9
epdu <epdu@temple.edu>
2024-02-08 16:03:26 -0500

    02082024lenovo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV3.java
@@ -27,6 +27,7 @@ import org.opencv.core.Scalar;
 import org.openftc.easyopencv.OpenCvCamera;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+//april tag does not work
 @Autonomous(name = "Auto Vision Portal init V3 ")
 public class AutonomousV3 extends LinearOpMode {
     HardwarePowerpuffs robot = new HardwarePowerpuffs();   // Use a Powerpuffs's hardware
