72cd35a5b364bbf7b8055a8d81e193dfb8de0f57
epdu <epdu@temple.edu>
2024-02-08 09:10:35 -0500

    02072024

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV.java
@@ -864,8 +864,9 @@ Returns the absolute orientation of the sensor as a set three angles with indica
 
         aprilTag = new AprilTagProcessor.Builder().build();
         redTeamPropOpenCv= new OpenCvVisionProcessor("Red", new Scalar(1, 50, 70), new Scalar(30, 255, 255) );
-        blueTeamPropOpenCv= new OpenCvVisionProcessor("Blue", new Scalar(90, 50,70), new Scalar(128, 255, 255) );
-//blue 128, 255, 255], [90, 50, 70]],
+        blueTeamPropOpenCv= new OpenCvVisionProcessor("Blue", new Scalar(210,100,100), new Scalar(270, 255, 255) );
+//        blueTeamPropOpenCv= new OpenCvVisionProcessor("Blue", new Scalar(90, 50,70), new Scalar(128, 255, 255) );
+// blue 128, 255, 255], [90, 50, 70]],
 //        'red1': [[180, 255, 255], [159, 50, 70]],
 //        'red2': [[9, 255, 255], [0, 50, 70]],
 //        blueTeamPropOpenCv= new OpenCvVisionProcessor("Blue", new Scalar(160, 200,120), new Scalar(100, 255, 255) );
