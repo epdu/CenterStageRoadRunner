51dcb37b3363c6df55b569e9a6b894ecdf111b74
epdu <epdu@temple.edu>
2024-02-10 15:12:40 -0500

    02082024lenovo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV3.java
@@ -576,11 +576,10 @@ public class AutonomousV3 extends LinearOpMode {
         telemetry.addData("yaw", yaw);
         telemetry.update();
         sleep(6000);
-
-        double leftFrontPower    =  x -y -yaw;
-        double rightFrontPower   =  x +y +yaw;
-        double leftBackPower     =  x +y -yaw;
-        double rightBackPower    =  x -y +yaw;
+        double leftFrontPower    =  x -y +yaw;
+        double rightFrontPower   =  x +y -yaw;
+        double leftBackPower     =  x +y +yaw;
+        double rightBackPower    =  x -y -yaw;
 
 /*        good for front camera
         double leftFrontPower    =  -x +y +yaw;
