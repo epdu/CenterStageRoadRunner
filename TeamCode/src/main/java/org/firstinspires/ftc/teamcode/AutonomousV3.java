b2ca17f9c8f092495ecdb0b7c24ca985d14d14ab
epdu <epdu@temple.edu>
2024-02-09 20:50:04 -0500

    02092024

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV3.java
@@ -550,7 +550,6 @@ public class AutonomousV3 extends LinearOpMode {
         double rightFrontPower   =  x +y -yaw;
         double leftBackPower     =  x +y +yaw;
         double rightBackPower    =  x -y -yaw;
-
         /*        good for front camera
         double leftFrontPower    =  -x +y +yaw;
         double rightFrontPower   =  -x -y -yaw;
