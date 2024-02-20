eb4ca11ff8f45b20825833573547aa02973c50e3
epdu <epdu@temple.edu>
2024-02-19 22:04:50 -0500

    02172024 testing Tele with apriltags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterstageTeleLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterstageTeleLinearOpMode.java
@@ -1,6 +1,5 @@
 //
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
