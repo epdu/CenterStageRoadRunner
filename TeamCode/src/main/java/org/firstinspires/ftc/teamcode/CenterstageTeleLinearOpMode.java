a809074be95442b997796214b37860ff90bb53c1
epdu <epdu@temple.edu>
2024-02-19 22:12:50 -0500

    02172024 testing Tele with apriltags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterstageTeleLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterstageTeleLinearOpMode.java
@@ -1,22 +1,21 @@
 //
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "A CenterstageTele LinearOpMode ")
 public class CenterstageTeleLinearOpMode  extends LinearOpMode {
     HardwarePowerpuffs robot = new HardwarePowerpuffs();
